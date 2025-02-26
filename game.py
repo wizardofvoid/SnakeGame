29071b32300ea7be23c5f24ea27a44736e9400ff
wizardofvoid <sarafa736@gmail.com>
2025-02-26 12:47:34 +0530

    Added some comments and time logic

--- game.py
+++ game.py
@@ -6,7 +6,6 @@ green = (0,255,0)
 black = (0,0,0)
 block_size = 20
 
-# def draw_snake(snake_body):
     
 
 def main():
@@ -19,9 +18,17 @@ def main():
     #Game loop
     running = True
     while running:
+        #Color the game world
         screen.fill(black)
-        pygame.draw.rect(screen, green, pygame.Rect(block_size,block_size,block_size,block_size))
+        
+        #Get the ticks to show the time user has been playing the game
+        ticks = (int)(pygame.time.get_ticks() / 1000)
+        print(ticks)
+        ticks += 1
+        
         pygame.display.flip()
+        
+        #Exit sequence
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
