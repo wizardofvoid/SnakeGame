706d2e4affe72f22a50d9faba184066f68336208
wizardofvoid <sarafa736@gmail.com>
2025-02-27 22:54:46 +0530

    Standardization

--- game.py
+++ game.py
@@ -1,29 +1,33 @@
 import pygame
 
+width = 1500
+height = 700
 #defining the colors and grid
 white = (255,255,255)
 green = (0,255,0)
 black = (0,0,0)
 block_size = 20
 
-    
+def makeSnake(size):
+    pygame.draw.rect(screen, green, [], 0)
 
 def main():
     
     #intitializing game window
     pygame.init()
-    screen = pygame.display.set_mode((600,400),pygame.RESIZABLE)
+    screen = pygame.display.set_mode((width,height),pygame.RESIZABLE)
     pygame.display.set_caption("Snake Game")
+    
     
     #Game loop
     running = True
     while running:
-        #Color the game world
+        #Build the game world(Frontend)
         screen.fill(black)
         
+        keys = pygame.key.get_pressed()
         #Get the ticks to show the time user has been playing the game
         ticks = (int)(pygame.time.get_ticks() / 1000)
-        print(ticks)
         ticks += 1
         
         pygame.display.flip()
